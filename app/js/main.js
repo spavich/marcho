4f4bde9e929367b3833a49953990099068648b07
Sasha Paraskevych <sasha.paraskevych@gmail.com>
2022-09-05 20:35:20 +0300

    feat: add contact page

--- app/js/main.js
+++ app/js/main.js
@@ -82,65 +82,7 @@ $(function(){
 		$($(this).attr('href')).addClass('product-tabs__content-item--active');
 
 	});
-});
 
-
-// Timer
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds
-  };
-}
-function initializeClock(id, endtime) {
-  const clock = document.querySelector('.promo__clock');
-  const daysSpan = clock.querySelector('.promo__days');
-  const hoursSpan = clock.querySelector('.promo__hours');
-  const minutesSpan = clock.querySelector('.promo__minutes');
-  const secondsSpan = clock.querySelector('.promo__seconds');
-
-  function updateClock() {
-    const t = getTimeRemaining(endtime);
-
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-    if (t.total <= 0) {
-      clearInterval(timeinterval);
-    }
-  }
-
-  updateClock();
-  const timeinterval = setInterval(updateClock, 1000);
-}
-
-const deadline = $('.promo__clock').attr('data-time');
-initializeClock('promo__clock', deadline);
-
-
-// Counter
-const minus = document.querySelector('.counter__minus'),
-			plus = document.querySelector('.counter__plus'),
-			num = document.querySelector('.counter__num');
-
-plus.addEventListener('click', ()=>{
-	if(num.value < 99 && num.value > 0){
-		num.value++;
-	}
-});
-minus.addEventListener('click', ()=>{
-	if(num.value > 1){
-		num.value--;
-	}
+	// Map
+	
 });
